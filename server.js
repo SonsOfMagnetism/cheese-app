6848cf4434e9368917b684ad365b54bd67a3ccde
Rene Santiago <rsj510@gmail.com>
2023-01-07 20:07:38 -0500

    completed backend

--- server.js
+++ server.js
@@ -1,6 +1,69 @@
 require("dotenv").config()
-const { PORT = 3000 } = process.env
+const { PORT = 3000, DATABASE_URL } = process.env
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+const cors = require("cors")
+const morgan = require("morgan")
 
-// app.get("/", (req, res) =?)
+mongoose.connect(DATABASE_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+})
+
+mongoose.connection
+    .on("open", () => console.log("You are connected to mongoose"))
+    .on("close", () => console.log("You are disconnected from mongoose"))
+    .on("error", (error) => console.log(error))
+
+const CheeseSchema = new mongoose.Schema({
+    name: String,
+    countryOfOrigin: String,
+    image: String
+})
+
+const Cheese = mongoose.model("Cheese", CheeseSchema)
+
+app.use(cors())
+app.use(morgan("dev"))
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.get("/cheese", async (req, res) => {
+    try {
+        res.json(await Cheese.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.post("/cheese", async (req, res) => {
+    try {
+        res.json(await Cheese.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.put("/cheese/:id", async (req, res) => {
+    try {
+        res.json(
+            await Cheese.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        )
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.delete("/cheese/:id", async (req, res) => {
+    try {
+        res.json(await Cheese.findById(req.params.id))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
